7b6f7c30e8929a97852bb22126261c82a52f1772
Luis Gustavo Polio <guspolio@outlook.es>
2022-03-05 12:58:02 -0600

    Agregar

--- Pantallas/Eliminar.js
+++ Pantallas/Eliminar.js
@@ -1,16 +1,12 @@
+import {React} from 'react';
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, SafeAreaView, FlatList,} from 'react-native';
+import {db} from '../server/Conexión';
+import {collection, getDocs} from 'firebase/firestore';
 
- function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Eliminar!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+ export default function App() {
+  const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -19,4 +15,32 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+async function obtenerDatos (){
+  const querySnapshot = await getDocs(collection(db, "Trabajadores"));
+querySnapshot.forEach((doc) => {
+  // doc.data() is never undefined for query doc snapshots
+  console.log(doc.id, " => ", doc.data());
+});
+}
+  
+  
+  return (
+    <View style={styles.container}>
+      <Text>Eliminar</Text>
+      <StatusBar style="auto" />
+
+      <TextInput
+              style={styles.input}
+              placeholder = "Ingresa Id"
+              onChangeText={(value)=>capturar('Apellidos', value) }
+              />
+
+
+      <Button title = "Agregar" onPress={()=>eliminar()}>Eliminar</Button>
+      <Button title = "Agregar" onPress={()=>obtenerDatos()}>Obtener datos</Button>
+    </View>
+  );
+}
+
+
+
--- Pantallas/Escribir.js
+++ Pantallas/Escribir.js
@@ -4,5 +4,64 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import {db} from '../server/Conexión';
 import {collection, addDoc} from 'firebase/firestore';
+import { async } from '@firebase/util';
 
+export default function App () {
+    const styles = StyleSheet.create({
+        input: {
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+        },
+      });
+      
+      const [state, setstate] = useState ({
+          Apellidos: '',
+          Nombre:'',
+          Telefono: '',
+      }) 
 
+      const capturar = (atrib,valor) => {
+          setstate({...state,[atrib]:valor})
+      }
+
+      async function agregar (){
+          if(state.Apellidos === '' | state.Nombre === '' | state.Telefono === ''){
+              alert ('Llena todos los datos antes de agregar un producto')
+          } else{
+              const Nombre = (state.Nombre)
+              const Apellidos = (state.Apellidos)
+              const Telefono = (state.Telefono)
+              await addDoc (collection(db, "Trabajadores"),{
+                  Apellidos: Apellidos,
+                  Nombre: Nombre,
+                  Telefono: Telefono,
+              });
+              alert('Se agregó con éxito')
+          }
+      }
+
+      return(
+          <View style = {styles.container}>
+              <Text>Agrega la información</Text>
+              <TextInput
+              style={styles.input}
+              placeholder = "Apellidos"
+              onChangeText={(value)=>capturar('Apellidos', value) }
+              />
+              <TextInput
+              style={styles.input}
+              placeholder = "Nombre"
+              onChangeText={(value)=>capturar('Nombre', value) }
+              />
+              <TextInput
+              style={styles.input}
+              placeholder = "Teléfono"
+              onChangeText={(value)=>capturar('Telefono', value) }
+              />
+              <Button title = "Agregar" onPress={()=>agregar()}> Agregar</Button>
+          </View>
+      )
+
+}
